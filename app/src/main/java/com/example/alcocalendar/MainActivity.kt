dc5091d8ffc65228078252ba8eb17788814c93a6
CAN4red <CAN4red@yandex.ru>
2024-09-24 15:41:39 +0300

    First push

--- app/src/main/java/com/example/alcocalendar/MainActivity.kt
+++ app/src/main/java/com/example/alcocalendar/MainActivity.kt
@@ -6,30 +6,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.alcocalendar.ui.calendar.month.MonthLayout
-import com.example.alcocalendar.ui.calendar.year.YearLayout
-import com.example.alcocalendar.ui.navigation.CalendarScreen
 import com.example.alcocalendar.ui.theme.AlcoCalendarTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,9 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AlcoCalendarTheme {
                 val navController = rememberNavController()
-                Scaffold(
-//                    bottomBar = { BottomNavigationBar(navController = navController) }
-                ) { innerPadding ->
+                Scaffold{ innerPadding ->
                     AlcoCalendarApp(
                         navController = navController,
                         modifier = Modifier.padding(innerPadding)
